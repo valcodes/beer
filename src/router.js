85adb5f2e2e5d3d33fa302dbe550fb5638301cfb
valentina abramova <valentina@v.local>
2017-11-21 12:03:45 -0600

    deleted extra file

--- src/router.js
+++ src/router.js
@@ -5,13 +5,11 @@ import LandingPage from "./components/LandingPage/LandingPage";
 import Favorite from "./components/FavoritesList/Favorite";
 import Random from "./components/FavoritesList/Random";
 import Favorites from "./components/Profile/Favorites";
-import RandomBeer from "./components/RandomBeer/RandomBeer";
 
 export default (
   <Switch>
     <Route exact path="/" component={LandingPage} />
     <Route path="/random" component={Random} />
     <Route path="/Favorites/:id" component={Favorites} />
-    <Route path="/random" component={RandomBeer} />
   </Switch>
 );
