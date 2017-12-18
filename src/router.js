e89cb2f42c7e9e019bb07e7216e2d8f6ee4f83fb
valentina abramova <valentina@v.local>
2017-12-18 11:54:04 -0600

    chore: added new api, need to add search function

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Switch, Route } from "react-router-dom";
-// import Newapi from "./components/Newapi";
+import Newapi from "./components/Newapi";
 import LandingPage from "./components/LandingPage/LandingPage";
 import ShoppingCart from "./components/ShoppingCart";
 import Random from "./components/FavoritesList/Random";
@@ -11,11 +11,11 @@ import Random3 from "./components/FavoritesList/Favs3";
 export default (
   <Switch>
     <Route exact path="/" component={LandingPage} />
-    <Route path="/random" component={Random} />
+    {/* <Route path="/random" component={Random} /> */}
     <Route path="/Favorites" component={Favorites} />
     <Route path="/cart" component={ShoppingCart} />
-    {/* <Route path="/new" component={Newapi} />  ISH DONT WORK MON*/}
-    <Route path="/random2" component={Random2} />
-    <Route path="/random3" component={Random3} />
+    <Route path="/new" component={Newapi} />
+    {/* <Route path="/random2" component={Random2} />
+    <Route path="/random3" component={Random3} /> */}
   </Switch>
 );
